f9af0f5e91f4daed51b90de5e5146e2bf3e78a42
facu-olivam <2406097@ucc.edu.ar>
2024-06-19 00:40:27 -0300

    Format Document. Closes #23

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Selecciona el canvas y su propiedad context
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
 
 // Definir la imagen de la nave espacial
 const spaceship = new Image();
-spaceship.src = './images/paracaidas-canvas.png'; // Ruta de la imagen de la nave espacial
+spaceship.src = "./images/paracaidas-canvas.png"; // Ruta de la imagen de la nave espacial
 
 /**
  * Ajustar el tamaño del canvas
@@ -27,20 +27,20 @@ const acceleration = 0.1;
  * @method calcResult
  */
 const calcResult = () => {
-  let distance = parseFloat(document.getElementById('distance').value);
-  let velocity = parseFloat(document.getElementById('velocity').value);
-  let time = parseFloat(document.getElementById('time').value);
+  let distance = parseFloat(document.getElementById("distance").value);
+  let velocity = parseFloat(document.getElementById("velocity").value);
+  let time = parseFloat(document.getElementById("time").value);
 
   // Validar que se hayan completado exactamente 2 de los 3 campos
   const fields = [distance, velocity, time];
-  const completedFields = fields.filter(field => !isNaN(field));
-  const greaterThanZeroFields = fields.filter(field => field > 0);
+  const completedFields = fields.filter((field) => !isNaN(field));
+  const greaterThanZeroFields = fields.filter((field) => field > 0);
 
   if (completedFields.length !== 2) {
     Toastify({
       text: "Debe completar exactamente 2 de los 3 campos.",
       background: "linear-gradient(to right, #FF9800, #F44336)",
-      duration: 3000
+      duration: 3000,
     }).showToast();
     return;
   }
@@ -49,7 +49,7 @@ const calcResult = () => {
     Toastify({
       text: "Los valores deben ser mayores a 0.",
       background: "linear-gradient(to right, #FF9800, #F44336)",
-      duration: 3000
+      duration: 3000,
     }).showToast();
     return;
   }
@@ -58,15 +58,15 @@ const calcResult = () => {
   let missingField;
   if (isNaN(distance)) {
     missingField = velocity * time;
-    document.getElementById('distance').value = missingField;
+    document.getElementById("distance").value = missingField;
     distance = missingField;
   } else if (isNaN(velocity)) {
     missingField = distance / time;
-    document.getElementById('velocity').value = missingField;
+    document.getElementById("velocity").value = missingField;
     velocity = missingField;
   } else {
     missingField = distance / velocity;
-    document.getElementById('time').value = missingField;
+    document.getElementById("time").value = missingField;
     time = missingField;
   }
 
@@ -90,10 +90,10 @@ const animate = () => {
   resizeCanvas();
 
   // Calcular el tiempo de caída
-  const time = parseFloat(document.getElementById('time').value);
+  const time = parseFloat(document.getElementById("time").value);
 
   // Calcular la velocidad inicial
-  const initialVelocity = parseFloat(document.getElementById('velocity').value);
+  const initialVelocity = parseFloat(document.getElementById("velocity").value);
 
   // Ajustar la velocidad inicial a la velocidad calculada
   if (!isNaN(initialVelocity)) {
@@ -150,7 +150,12 @@ const drawVelocityArrow = (x, y, size) => {
   ctx.font = "12px Arial";
   ctx.fillStyle = "#000";
   ctx.textAlign = "center";
-  ctx.fillText(`Velocidad: ${velocity.toFixed(2)} ${unitInit}/${unitInit == "km" ? "h" : "s"}`, x, y - size - 5);
+  ctx.fillText(
+    `Velocidad: ${velocity.toFixed(2)} ${unitInit}/${unitInit == "km" ? "h" : "s"
+    }`,
+    x,
+    y - size - 5
+  );
 };
 
 /**
@@ -167,21 +172,25 @@ const drawScale = () => {
   ctx.strokeStyle = "#000";
   ctx.fillStyle = "#000";
   ctx.font = "12px Arial";
-  
+
   for (let i = 0; i < numLabels; i++) {
-    const distanceValue = parseFloat(document.getElementById('distance').value);
+    const distanceValue = parseFloat(document.getElementById("distance").value);
     const label = (distanceValue / (numLabels - 1)) * i;
     const yPos = startY - deltaY * i;
     ctx.beginPath();
     ctx.moveTo(canvas.width - scaleHeight, yPos);
     ctx.lineTo(canvas.width, yPos);
     ctx.stroke();
-    ctx.fillText(label.toFixed(2) + unitInit, canvas.width - scaleHeight - 60, yPos + 5); // Ajuste para dejar espacio entre el texto y el borde del canvas
+    ctx.fillText(
+      label.toFixed(2) + unitInit,
+      canvas.width - scaleHeight - 60,
+      yPos + 5
+    ); // Ajuste para dejar espacio entre el texto y el borde del canvas
   }
 };
-  
+
 // Unidad de medida inicial
-let unitInit = 'km';
+let unitInit = "km";
 
 /**
  * Función para convertir unidades
@@ -195,20 +204,21 @@ let unitInit = 'km';
 const convertUnits = (value, currentUnit, targetUnit, conversionType) => {
   // Definir factores de conversión para distancia, tiempo y velocidad
   const conversionFactors = {
-    'm': {
-      'distance': { 'km': 0.001, 'm': 1 },
-      'time': { 'km': 1/3600, 'm': 1/60 }, 
-      'velocity': { 'km': 3.6, 'm': 1}
+    m: {
+      distance: { km: 0.001, m: 1 },
+      time: { km: 1 / 3600, m: 1 / 60 },
+      velocity: { km: 3.6, m: 1 },
     },
-    'km': {
-      'distance': { 'm': 1000, 'km': 1 }, 
-      'time': { 'm': 3600, 'km': 1 }, 
-      'velocity': { 'm': 1/3.6, 'km': 1}
-    }
+    km: {
+      distance: { m: 1000, km: 1 },
+      time: { m: 3600, km: 1 },
+      velocity: { m: 1 / 3.6, km: 1 },
+    },
   };
 
   // Realizar la conversión
-  const conversionFactor = conversionFactors[currentUnit][conversionType][targetUnit];
+  const conversionFactor =
+    conversionFactors[currentUnit][conversionType][targetUnit];
   const convertedValue = value * conversionFactor;
 
   return convertedValue;
@@ -220,36 +230,53 @@ const convertUnits = (value, currentUnit, targetUnit, conversionType) => {
  */
 const unitConverterChange = () => {
   // Obtener los valores actuales de los inputs
-  const distanceValue = parseFloat(document.getElementById('distance').value);
-  const timeValue = parseFloat(document.getElementById('time').value);
-  const velocityValue = parseFloat(document.getElementById('velocity').value);
+  const distanceValue = parseFloat(document.getElementById("distance").value);
+  const timeValue = parseFloat(document.getElementById("time").value);
+  const velocityValue = parseFloat(document.getElementById("velocity").value);
 
   // Obtener la unidad de medida a la que se desea convertir
-  const convertUnit = document.getElementById('unit-converter').value;
+  const convertUnit = document.getElementById("unit-converter").value;
 
   // Validar que los valores sean válidos
-  if (isNaN(distanceValue) || isNaN(timeValue) || isNaN(velocityValue) || distanceValue === 0 || timeValue === 0 || velocityValue === 0) {
+  if (
+    isNaN(distanceValue) ||
+    isNaN(timeValue) ||
+    isNaN(velocityValue) ||
+    distanceValue === 0 ||
+    timeValue === 0 ||
+    velocityValue === 0
+  ) {
     unitInit = convertUnit;
     Toastify({
       text: "Por favor, ingrese valores válidos para la conversión.",
       background: "linear-gradient(to right, #FF9800, #F44336)",
-      duration: 3000
+      duration: 3000,
     }).showToast();
     return;
   }
 
   // Convertir los valores a la nueva unidad
-  const convertedDistance = convertUnits(distanceValue, unitInit, convertUnit, 'distance');
-  const convertedTime = convertUnits(timeValue, unitInit, convertUnit, 'time');
-  const convertedVelocity = convertUnits(velocityValue, unitInit, convertUnit, 'velocity');
+  const convertedDistance = convertUnits(
+    distanceValue,
+    unitInit,
+    convertUnit,
+    "distance"
+  );
+  const convertedTime = convertUnits(timeValue, unitInit, convertUnit, "time");
+  const convertedVelocity = convertUnits(
+    velocityValue,
+    unitInit,
+    convertUnit,
+    "velocity"
+  );
 
   // Actualizar el valor de unitInit para guardar su estado cuando el select cambie nuevamente
   unitInit = convertUnit;
 
   // Actualizar los valores de los inputs
-  document.getElementById('distance').value = convertedDistance.toFixed(2);
-  document.getElementById('time').value = convertedTime.toFixed(2);
-  document.getElementById('velocity').value = convertedVelocity.toFixed(2);
+  document.getElementById("distance").value = convertedDistance.toFixed(2);
+  document.getElementById("time").value = convertedTime.toFixed(2);
+  document.getElementById("velocity").value = convertedVelocity.toFixed(2);
 };
 
 /**
@@ -271,18 +298,18 @@ const validateInput = (event) => {
     Toastify({
       text: "Solo se permiten números y un punto decimal.",
       background: "linear-gradient(to right, #FF9800, #F44336)",
-      duration: 3000
+      duration: 3000,
     }).showToast();
     return;
   }
 
   // Permitir solo un punto decimal en el valor
-  if (key === '.' && value.includes('.')) {
+  if (key === "." && value.includes(".")) {
     event.preventDefault();
     Toastify({
       text: "Solo se permite un punto decimal.",
       background: "linear-gradient(to right, #FF9800, #F44336)",
-      duration: 3000
+      duration: 3000,
     }).showToast();
     return;
   }
@@ -293,7 +320,7 @@ const validateInput = (event) => {
     Toastify({
       text: "El valor no puede tener más de 8 dígitos.",
       background: "linear-gradient(to right, #FF9800, #F44336)",
-      duration: 3000
+      duration: 3000,
     }).showToast();
   }
 };
@@ -305,7 +332,7 @@ const validateInput = (event) => {
  */
 const validatePastedInput = (event) => {
   const input = event.target;
-  const paste = (event.clipboardData || window.clipboardData).getData('text');
+  const paste = (event.clipboardData || window.clipboardData).getData("text");
   const newValue = input.value + paste;
 
   // Permitir solo números y un punto decimal
@@ -315,7 +342,7 @@ const validatePastedInput = (event) => {
     Toastify({
       text: "Solo se permiten números positivos y un punto decimal.",
       background: "linear-gradient(to right, #FF9800, #F44336)",
-      duration: 3000
+      duration: 3000,
     }).showToast();
     return;
   }
@@ -326,7 +353,7 @@ const validatePastedInput = (event) => {
     Toastify({
       text: "El valor no puede tener más de 8 dígitos.",
       background: "linear-gradient(to right, #FF9800, #F44336)",
-      duration: 3000
+      duration: 3000,
     }).showToast();
   }
 };
@@ -340,11 +367,11 @@ const handleInput = (event) => {
   const value = event.target.value;
   const regex = /^[0-9]*\.?[0-9]*$/;
   if (!regex.test(value)) {
-    event.target.value = value.replace(/[^0-9.]/g, '');
+    event.target.value = value.replace(/[^0-9.]/g, "");
     Toastify({
       text: "Solo se permiten números positivos y un punto decimal.",
       background: "linear-gradient(to right, #FF9800, #F44336)",
-      duration: 3000
+      duration: 3000,
     }).showToast();
   }
 };
